2b925e0c6410ab6db7ecdd21e2ac6f72a763a3b0
Zeyad Dief <ziaddeveloper15@gmail.com>
2023-01-30 23:34:58 +0200

    Random Body Background

---
+++ Random Body Background/master.js
@@ -0,0 +1,8 @@
+var btn = document.querySelector(".btn");
+
+btn.addEventListener("click", randomBg);
+
+function randomBg() {
+  document.body.style.backgroundColor =
+    "#" + Math.random().toString(16).slice(2, 8);
+}
